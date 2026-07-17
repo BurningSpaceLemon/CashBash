4292606be16f212ba68071c02907f97d53877833
BurningSpaceLemon <nils_brueckner@outlook.de>
2026-07-17 23:08:08 +0200

    Update sw.js V6

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
    - Optionally bump the query param in register('./sw.js?v=...') too
 */
 
-const SW_VERSION = 'v5';
+const SW_VERSION = 'v6';
 const CACHE_STATIC = `cashbash-static-${SW_VERSION}`;
 const CACHE_PAGES  = `cashbash-pages-${SW_VERSION}`;
 const KEEP_CACHES = new Set([CACHE_STATIC, CACHE_PAGES]);
